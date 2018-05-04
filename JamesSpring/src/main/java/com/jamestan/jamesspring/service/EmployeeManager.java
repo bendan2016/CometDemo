41cded0cf98f8232808975f4cf15db4a08a5e67a
bendan2016 <james.tan.1@gmail.com>
2018-05-04 11:04:28 -0400

    create

---
+++ JamesSpring/src/main/java/com/jamestan/jamesspring/service/EmployeeManager.java
@@ -0,0 +1,29 @@
+package com.jamestan.jamesspring.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.jamestan.jamesspring.model.*;
+ 
+public class EmployeeManager {
+	private static List<Employee> employeeList;
+ 
+	public EmployeeManager(){
+		 employeeList = new ArrayList<Employee>();
+		 employeeList.add(new Employee(1, "Mike", "Smith"));
+		 employeeList.add(new Employee(2, "John", "Taylor"));
+		 employeeList.add(new Employee(3, "Dave", "Wilson"));	 
+	}
+ 
+	public List<Employee> getEmployeeList(){
+		return employeeList;
+	}
+	
+	public Employee addEmployee(Employee e){
+		Employee employee = new Employee();
+		employee.setId(employeeList.size()+1);
+		employee.setFirstName(e.getFirstName());
+		employee.setLastName(e.getLastName());
+		employeeList.add(employee);
+		return employee;
+	}
+}
